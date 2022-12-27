2dc93654a8527efc63ae5734da5b1ff01441b883
Sergey Batukov <sashachepuha@gmail.com>
2022-12-27 14:58:08 +0700

    update

--- packages/client/src/components/ui/Button.vue
+++ packages/client/src/components/ui/Button.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-button.button(:class='classes')
+button.button(:class='classes' v-bind='$attrs')
   slot   
 </template>
 <script lang="ts" setup>
@@ -8,30 +8,48 @@ import { computed, type PropType } from "vue";
 const props = defineProps({
   size: { type: String as PropType<"s" | "m" | "l">, default: "m" },
   mode: {
-    type: String as PropType<"default" | "ghost" | "danger">,
+    type: String as PropType<"default" | "fill" | "ghost" | "danger" | "link">,
     default: "default",
   },
-  active: { type: Boolean as PropType<boolean>, default: false },
-  disable: { type: Boolean as PropType<boolean>, default: false },
+  accent: { type: Boolean as PropType<boolean>, default: false },
+  danger: { type: Boolean as PropType<boolean>, default: false },
 });
 
 const classes = computed(() => [
   `button_${props.mode}`,
   `button_${props.mode}__${props.size}`,
-  props.active ? `button_${props.mode}__active` : null,
-  props.disable ? `button_${props.mode}__disable` : null,
+  props.accent ? `button_${props.mode}__accent` : "",
+  props.danger ? `button_${props.mode}__danger` : "",
 ]);
 </script>
 <style lang="sass">
 .button
+  transition: all 0.3s ease
   cursor: pointer
   display: flex
   border: none
+  outline: none
   max-width: max-content
+
+  &:active
+    transform: scale(0.98)
+
+  &:disabled
+    cursor: not-allowed
+    opacity: 0.5
+
   &_default
     border-radius: 4px
     background: var(--color-background-300)
     color: var(--color-text-200)
+    &__danger
+      background: var(--color-background-300)
+      color: var(--error-100)
+      &:hover
+        background: var(--error-100) !important
+
+    &__danger + &__accent
+      background: var(--error-100)
     &__s
       padding: 4px 8px
       font-size: 12px
@@ -41,16 +59,59 @@ const classes = computed(() => [
     &__l
       font-size: 16px
       padding: 12px 20px
-    &:hover
+    &:hover, &__accent
       background: var(--accent-100)
       color: var(--color-text-100)
-    &__active
-      background: var(--accent-200)
-      color: var(--accent-100)
-    &__disable
-      cursor: not-allowed
-      opacity: 0.5
+
+    &:focus-visible
+      background: var(--accent-200) !important
+      color: var(--accent-100) !important
+      outline: 2px solid var(--accent-100)
+
+  &_link
+    background: none
+    color: var(--accent-100)
+    border-bottom: 1px solid transparent
+    &__danger
+      color: var(--error-100)
       &:hover
-        background: var(--color-background-100)
-        color: var(--color-text-300)
+        border-color: var(--error-100) !important
+    &__danger + &__accent
+      border-color: var(--error-100)
+
+    &__s
+      font-size: 12px
+      padding: 4px 0
+    &__m
+      font-size: 14px
+      padding: 8px 0
+    &__l
+      font-size: 16px
+      padding: 12px 0
+    &:hover, &:focus-visible, &__accent
+      border-color: var(--accent-100)
+
+  &_ghost
+    background: var(--accent-200)
+    color: var(--accent-100)
+    border-radius: 4px
+    border: 2px solid transparent
+    &__danger
+      color: var(--error-100)
+      background: var(--error-200)
+      &:hover
+        border-color: var(--error-200) !important
+    &__danger + &__accent
+      border-color: var(--error-200)
+    &__s
+      font-size: 12px
+      padding: 4px 8px
+    &__m
+      font-size: 14px
+      padding: 8px 12px
+    &__l
+      font-size: 16px
+      padding: 12px 20px
+    &:hover, &:focus-visible, &__accent
+      border-color: var(--accent-200)
 </style>
--- packages/client/src/views/HomeView.vue
+++ packages/client/src/views/HomeView.vue
@@ -1,15 +1,47 @@
 <template lang="pug">
 main.home
-  h1 HOME
+  //- h1 HOME
+  img.home_logo(src='@/assets/logo.svg' width='100')
+  h1 Muup Typography
+  h2 Header 2
+  h3 Header 3
+  h4 Header 4
+  h5 Header 5
+  p Lorem Ipsum is simply dummy text of the #[mark printing and typesetting industry]. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  blockquote Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  span span
+  cite cite
+  mark mark
+  code CODE wef wefp ewkp fkewpf kwepoof kewpo kwepk 
+  label label
+  h2 Button
   UIButton(size='s') default size s
   UIButton(size='m') default size m
   UIButton(size='l') default size l
-  UIButton(size='s' active) default size s active
-  UIButton(size='m' active) default size m active
-  UIButton(size='l' active) default size l active
-  UIButton(size='s' disable) default size s disable
-  UIButton(size='m' disable) default size m disable
-  UIButton(size='l' disable) default size l disable
+  UIButton(size='l' accent) default size l accent
+  UIButton(size='l' danger) default size l danger
+  UIButton(size='l' danger accent) default size l danger accent
+  UIButton(size='s' disabled) default size s disable
+  UIButton(size='m' disabled) default size m disable
+  UIButton(size='l' disabled) default size l disable
+  UIButton(mode='link' size='s') default size s
+  UIButton(mode='link' size='m') default size m
+  UIButton(mode='link' size='l') default size l
+  UIButton(mode='link' size='l' accent) default size l accent
+  UIButton(mode='link' size='l' danger) default size l danger
+  UIButton(mode='link' size='l' danger accent) default size l danger accent
+  UIButton(mode='link' size='s' disabled) default size s disable
+  UIButton(mode='link' size='m' disabled) default size m disable
+  UIButton(mode='link' size='l' disabled) default size l disable
+  UIButton(mode='ghost' size='s') default size s
+  UIButton(mode='ghost' size='m') default size m
+  UIButton(mode='ghost' size='l') default size l
+  UIButton(mode='ghost' size='l' accent) default size l
+  UIButton(mode='ghost' size='l' danger) default size l danger
+  UIButton(mode='ghost' size='l' danger accent) default size l danger accent
+  UIButton(mode='ghost' size='s' disabled) default size s disable
+  UIButton(mode='ghost' size='m' disabled) default size m disable
+  UIButton(mode='ghost' size='l' disabled) default size l disable
 </template>
 <script setup lang="ts"></script>
 <style lang="sass">
@@ -18,4 +50,6 @@ main.home
   flex-direction: column
   gap: 20px
   padding: 40px
+  height: 100%
+  width: 100%
 </style>
